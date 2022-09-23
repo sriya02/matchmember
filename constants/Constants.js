6ee194637419cbc6806e2c02f6669800d883a6f9
sriya02 <114263280+sriya02@users.noreply.github.com>
2022-09-23 14:38:55 -0700

    Initial commit

---
+++ constants/Constants.js
@@ -0,0 +1,17 @@
+const nameToPic = {
+	amolbudhiraja: ["Amol Budhiraja", require("../assets/MemberPictures/amolbudhiraja.png")],
+	amyxu: ["Amy Xu", require("../assets/MemberPictures/amyxu.png")],
+	aryanvichare: ["Aryan Vichare", require("../assets/MemberPictures/aryanvichare.jpeg")],
+	elisakim: ["Elisa Kim", require("../assets/MemberPictures/elisakim.jpeg")],
+	emilyholmes: ["Emily Holmes", require("../assets/MemberPictures/emilyholmes.jpg")],
+	ericyang: ["Eric Yang", require("../assets/MemberPictures/ericyang.png")],
+	ethankuo: ["Ethan Kuo", require("../assets/MemberPictures/ethankuo.jpg")],
+	jenniferzhao: ["Jennifer Zhao", require("../assets/MemberPictures/jenniferzhao.jpeg")],
+	josephtennyson: ["Joseph Tennyson", require("../assets/MemberPictures/josephtennyson.jpg")],
+	kevinlin: ["Kevin Lin", require("../assets/MemberPictures/kevinlin.jpg")],
+	maxinetseng: ["Maxine Tseng", require("../assets/MemberPictures/maxinetseng.jpg")],
+	nadiamohammadgholibaigi: ["Nadia Mohammad Gholi Baigi", require("../assets/MemberPictures/nadiamohammadgholibaigi.png")],
+	sameerkeswani: ["Sameer Keswani", require("../assets/MemberPictures/sameerkeswani.jpg")],
+	sriyatallapragada: ["Sriya Tallapragada", require("../assets/MemberPictures/sriyatallapragada.jpg")]
+}
+export { nameToPic }
